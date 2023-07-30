07468679dd8273d0464f71ef769166fd5bc96534
ankur-2103 <85174725+ankur-2103@users.noreply.github.com>
2023-07-30 20:28:39 +0530

    added auto-scroll

--- src/components/AccordionList.tsx
+++ src/components/AccordionList.tsx
@@ -1,12 +1,9 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import rawData from '../assets/celebrities.json';
 import Search from '../assets/search.svg';
 import Accordion from './Accordion';
 import DeleteDialog from './DeleteDialog';
 
-//  This file is used for creating list of accordion
-
-// Celebrites interface
 export interface Data{
     id: number,
     first: string,
@@ -19,18 +16,16 @@ export interface Data{
     description: string
 }
 
-// React functional component to render list of accordion 
 const AccordionList = () => {
 
-    // States for storing data
     const [data, setData] = useState<Data[]>(rawData);
     const [open, setOpen] = useState<boolean[]>(Array(rawData.length).fill(false));
     const [edit, setEdit] = useState<boolean>();
     const [deleteDialog, setDeleteDialog] = useState<boolean>(false);
     const [deleteItemId, setDeleteItemId] = useState<number>(0);
     const [search, setSearch] = useState<string>('');
+    const ref = useRef<HTMLSpanElement>(null);
 
-    // handle search
     useEffect(() => {
         const filterData = () => {
             if (search.length === 0) {
@@ -41,20 +36,17 @@ const AccordionList = () => {
             setData(newData.length === 0 ? rawData : newData);
         }
         filterData();
-    },[search, data])
+    },[search])
 
-    // handle edit state
     const handleEdit = (val:boolean) => {
         setEdit(val);
     }
 
-    // handle delete dialog
     const handleDelete = (id:number) => {
         setDeleteDialog(true);
         setDeleteItemId(id);
     }
     
-    // handle delete item
     const deleteItem = () => {
         setData(data.filter(item => item.id !== deleteItemId));
         setOpen(Array(data.length-1).fill(false))
@@ -62,7 +54,6 @@ const AccordionList = () => {
         setDeleteItemId(0);
     }
 
-    // handle open and close of accordion
     const handleOpen = (index: number) => {
         if (edit) {
             return;
@@ -75,9 +66,13 @@ const AccordionList = () => {
             }
         })
         setOpen(newArr);
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            ref.current?.scrollTo(0, 80 * index+1);
+        } else {
+            ref.current?.scrollTo(0, 65 * index);
+        }
     }
 
-    // handle update or edit of item
     const handleUpdate = (index:number, newData:Data) => {
         const newArr = data.map((item, i) => {
             if (i === index) {
@@ -95,7 +90,7 @@ const AccordionList = () => {
                 <img src={Search}/>
                 <input placeholder='Search' className='w-full outline-0' onChange={(e)=>setSearch(e.target.value)}/>
             </span>
-            <span className='flex flex-1 flex-col scroll-smooth overflow-auto gap-4 w-full md:w-fit items-center m-2 p-2 h-[100%]'>
+            <span className='flex flex-1 flex-col scroll-smooth overflow-auto gap-4 w-full md:w-fit items-center m-2 p-2 h-[100%]' ref={ref}>
                 {data.map((item, index) => <Accordion key={item.id} open={open[index]} handleOpen={handleOpen} index={index} item={item} handleEdit={handleEdit} handleUpdate={handleUpdate} handleDelete={handleDelete} />)}
             </span>
             <DeleteDialog isOpen={deleteDialog} handleDelete={deleteItem} close={()=>setDeleteDialog(false)} />
